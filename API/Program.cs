6567007e69ea624801b94724a3562d5f54acf090
chidi <huxleyberg@live.com>
2025-03-17 05:36:54 -0300

    client

--- API/Program.cs
+++ API/Program.cs
@@ -10,10 +10,16 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+// Load allowed CORS origins from configuration
+var allowedOrigins = builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins(allowedOrigins ?? new string[] { }));
+
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
